94204e407c36ddb473ed33d1382b53682923b34f
huy <canhoduclong@gmail.com>
2024-11-30 17:14:28 +0700

    Update side bar

--- shop1/sidebar-shop.php
+++ shop1/sidebar-shop.php
@@ -1,33 +1,32 @@
 <aside id="secondary" class="widget-area">
     <?php dynamic_sidebar('sidebar-1'); ?>
-    <div class="widget">
-        <h2 class="widget-title">Categories</h2>
-        <ul>
-            <?php
-            $categories = get_categories();
-            foreach ($categories as $category) {
-                echo '<li><a href="' . get_category_link($category->term_id) . '">' . $category->name . '</a></li>';
-            }
-            ?>
-        </ul>
-    </div>
-    <div class="widget">
-        <h2 class="widget-title">Tag</h2>
-        <?php
-        // Lấy danh sách 10 tags phổ biến nhất
-        $tags = get_terms(array(
-            'taxonomy' => 'post_tag',
-            'orderby' => 'count',
-            'order' => 'DESC',
-            'number' => 10,
-        ));
 
-        // Lặp qua từng tag và hiển thị
-        foreach ($tags as $tag) {
-            echo '<a href="' . get_tag_link($tag->term_id) . '">' . $tag->name . '</a><br>';
-        }
-        ?>
+
+    <div class="widget widget-collapsible">
+        <h3 class="widget-title">
+            <a data-toggle="collapse" href="#widget-1" role="button" aria-expanded="true" aria-controls="widget-1">Sản phẩm</a>
+        </h3>
+        <div class="collapse show" id="widget-1">
+            <div class="widget-body">
+                <div class="filter-items filter-items-count">
+                    <?php
+                    $categories = get_categories();
+                    foreach ($categories as $category) {?> 
+                        <div class="filter-item">
+                            <div class="custom-control custom-checkbox">
+                                <input type="checkbox" class="custom-control-input" value="dam-nu-tuoi-teen" id="dam-nu-tuoi-teen">
+                                <label class="custom-control-label" for="dam-nu-tuoi-teen"><?=$category->name?></label>
+                            </div>  
+                            <span class="item-count"><?=$category->count?></span>
+                        </div> 
+                    <?php }
+                    ?> 
+                </div><!-- End .filter-items -->
+            </div><!-- End .widget-body -->
+        </div><!-- End .collapse -->
     </div>
+ 
+  
 </aside>
 
  
